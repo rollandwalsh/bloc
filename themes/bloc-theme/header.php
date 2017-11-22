2c43670f634cffc63c88114db8b199a352b708c2
rollandwalsh <rollandwalsh@gmail.com>
2017-11-22 15:56:04 -0800

    Adds Font Awesome stylesheet

--- themes/bloc-theme/header.php
+++ themes/bloc-theme/header.php
@@ -15,6 +15,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	</head>
 	<body <?php body_class(); ?>>
 
